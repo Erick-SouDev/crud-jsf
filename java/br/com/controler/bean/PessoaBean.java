071ad9879f68bb587b1caf684e09a64c82bc3eb5
erickbrunopequenodossantos@outlook.com <erickbrunopequenodossantos@outlook.com>
2023-11-20 08:58:10 -0300

    alterado um trecho do codigo

--- java/br/com/controler/bean/PessoaBean.java
+++ java/br/com/controler/bean/PessoaBean.java
@@ -1,4 +1,4 @@
-package br.com.cursojsf;
+package br.com.controler.bean;
 
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
--- java/br/com/controler/bean/RelLancamento.java
+++ java/br/com/controler/bean/RelLancamento.java
@@ -1,4 +1,4 @@
-package br.com.cursojsf;
+package br.com.controler.bean;
 
 import java.io.Serializable;
 import java.util.ArrayList;
